40297086098ad5258cdc7d5e7274c5cbb86e26fb
unknown <alyhassanshahid@gmail.com>
2025-05-11 21:17:18 +0500

    Updated app file

--- medchatbot/embedder.py
+++
@@ -1,5 +0,0 @@
-# embedder.py
-from langchain_huggingface import HuggingFaceEmbeddings
-
-def get_embeddings():
-    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
--- medchatbot/requirements.py
+++
@@ -1,8 +0,0 @@
-# requirements.py
-def install_dependencies():
-    import subprocess
-    subprocess.run(["pip", "install", "--quiet", 
-        "langchain", "langchain-community", "langchain_groq", "sentence-transformers",
-        "unstructured", "pandas", "qdrant_client", "langchain_huggingface", 
-        "langchain_qdrant", "pypdf", "jq"
-    ])
